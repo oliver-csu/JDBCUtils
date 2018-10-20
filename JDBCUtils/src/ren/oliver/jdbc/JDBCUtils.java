b618b1d9e085b0b67af1b53139af18f18797ba1a
oliver-csu <oliver.csu@gmail.com>
2018-10-20 19:47:56 +0800

    ClassLoader

--- JDBCUtils/src/ren/oliver/jdbc/JDBCUtils.java
+++ JDBCUtils/src/ren/oliver/jdbc/JDBCUtils.java
@@ -1,31 +1,49 @@
 package ren.oliver.jdbc;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ResourceBundle;
+import java.util.Properties;
 
 /**
  * 提供获取连接和释放资源的 方法
  * 
+ * @author Never Say Never
+ * @date 2016年7月29日
+ * @version V1.0
  */
 public class JDBCUtils {
 	private static String driver;
 	private static String url;
 	private static String username;
 	private static String password;
-	
+
 	/**
 	 * 静态代码块加载配置文件信息
 	 */
-	static{
-		ResourceBundle bundle = ResourceBundle.getBundle("db");
-		driver = bundle.getString("driver");
-		url = bundle.getString("url");
-		username = bundle.getString("username");
-		password = bundle.getString("password");
+	static {
+		try {
+			// 1.通过当前类获取类加载器
+			ClassLoader classLoader = JDBCUtils.class.getClassLoader();
+			// 2.通过类加载器的方法获得一个输入流
+			InputStream is = classLoader.getResourceAsStream("db.properties");
+			// 3.创建一个properties对象
+			Properties props = new Properties();
+			// 4.加载输入流
+			props.load(is);
+			// 5.获取相关参数的值
+			driver = props.getProperty("driver");
+			url = props.getProperty("url");
+			username = props.getProperty("username");
+			password = props.getProperty("password");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	/**
